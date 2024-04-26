fa4942f41f5102c4378402014753b58c4889f46e
Makena Sandra <47531164+makenasandra@users.noreply.github.com>
2024-04-26 11:24:50 +0000

    added components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,6 +3,17 @@ import AlbumCard from "./AlbumCard";
 import { albums } from "../data/albums";
 
 function AlbumList() {
+  let albumArray = albums.map(album => {
+    const {name, image, genre} = album;
+    return(
+      <AlbumCard
+        name={name}
+        image={image}
+        genre={genre}
+      />
+    )
+  })
+
   return (
     <section className="albums">
       <AlbumCard
